03409cbfd52c094c4af63fd5ee3dd10cae2b8e10
Phi Huynh <trumhemcut@gmail.com>
2016-12-04 15:43:03 +0700

    Update temperature page

--- src/components/dash/Temperature.vue
+++ src/components/dash/Temperature.vue
@@ -2,58 +2,84 @@
   <section class='content'>
     <h1 class="text-center">Temperature</h1>
     <h4 class="text-center">Temperature & Hubmidity of our home</h4>
-
     <div class="row">
       <div class="col-md-12">
-        This is where the content of Temperature & Humidity showing
+        <div id="chart"></div>
       </div>
     </div>
   </section>
 </template>
 <script>
+import Rickshaw from 'rickshaw'
+import PubNub from 'pubnub'
+
 export default {
   name: 'Temperature',
   data: function () {
     return {
-      stats: [{
-        header: '8390',
-        text: 'Visitors'
-      }, {
-        header: '30%',
-        text: 'Referrals'
-      }, {
-        header: '70%',
-        text: 'Organic'
-      }]
     }
   },
   mounted: function () {
     this.$nextTick(function () {
-      // Add map
-      window.jQuery.getScript('/static/js/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js', function (data, textStatus, jqxhr) {
-        window.jQuery.getScript('/static/js/plugins/jvectormap/jquery-jvectormap-world-mill.js', function (data, textStatus, jqxhr) {
-          window.jQuery('#world-map-markers').vectorMap({
-            map: 'world_mill'
-          })
+      var tv = 5000
+
+      // instantiate our graph!
+      var graph = new Rickshaw.Graph({
+        element: document.getElementById('chart'),
+        width: 900,
+        height: 400,
+        renderer: 'line',
+        series: new Rickshaw.Series.FixedDuration([{ name: 'one' }], undefined, {
+          timeInterval: tv,
+          maxDataPoints: 100,
+          timeBase: new Date().getTime() / 1000
         })
+      })
+
+      graph.render()
+
+      var pubnub = new PubNub({
+        subscribeKey: 'sub-c-434796fa-b9e7-11e6-8036-0619f8945a4f',
+        publishKey: 'pub-c-038f9e11-fc17-4bf1-b2be-3946c20238fa',
+        ssl: true
+      })
+
+      var i = 0
+      setInterval(function () {
+        var data = { one: Math.floor(Math.random() * 40) + 120 }
+
+        var randInt = Math.floor(Math.random() * 100)
+        data.two = (Math.sin(i++ / 40) + 4) * (randInt + 400)
+        data.three = randInt + 300
+
+        pubnub.publish(
+          {
+            message: data,
+            channel: 'DHT'
+          },
+          function (status, response) {
+            console.log(response)
+          }
+        )
+      }, tv)
+
+      pubnub.addListener({
+        message: function (data) {
+          graph.series.addData(data.message)
+          graph.render()
+          console.log('New Message!!', data)
+        },
+        presence: function (presenceEvent) {
+          // handle presence
+        }
+      })
+      console.log('Subscribing...')
+      pubnub.subscribe({
+        channels: ['DHT']
       })
     })
   }
 }
 </script>
-
 <style>
-.fake { color: 'red'}
-#world-map-markers svg {
-  height: 355px;
-}
-.row.no-gutters {
-  margin-right: 0;
-  margin-left: 0;
-}
-.row.no-gutters > [class^="col-"],
-.row.no-gutters > [class*=" col-"] {
-  padding-right: 0;
-  padding-left: 0;
-}
 </style>
